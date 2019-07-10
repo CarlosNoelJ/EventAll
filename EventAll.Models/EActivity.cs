b50fdd77375b12f4a9bdac496fe4feb58c533434
Carlos Noel <carlosnoelg3@outlook.com>
2019-07-10 16:57:53 -0500

    Validators Done.

--- EventAll.Models/EActivity.cs
+++ EventAll.Models/EActivity.cs
@@ -8,7 +8,6 @@ namespace EventAll.Models
         public string DishName { get; set; }
         public double Price { get; set; }
         public int QuantityOfDishes { get; set; }
-        public int MyProperty { get; set; }
         public DateTime DateOfActivity { get; set; }
         
         public EPay EPay { get; set; }
---
+++ EventAll.Models/FluentValidator/EActivityValidator.cs
@@ -0,0 +1,47 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EventAll.Models.FluentValidator
+{
+    public class EActivityValidator : AbstractValidator<EActivity>
+    {
+        public EActivityValidator()
+        {
+            RuleFor(act => act.Id).NotEmpty();
+
+            RuleFor(act => act.DishName)
+                .Length(3,50)
+                .NotEmpty();
+
+            RuleFor(act => act.Price)
+                .NotEmpty()
+                .GreaterThan(0);
+
+            RuleFor(act => act.QuantityOfDishes)
+                .NotEmpty()
+                .GreaterThan(0);
+
+            RuleFor(act => act.DateOfActivity)
+                .NotEmpty();
+
+            RuleFor(act => act.EPay)
+                .NotEmpty()
+                .NotNull();
+
+            RuleFor(act => act.EPay)
+                .NotEmpty()
+                .NotNull();
+
+            RuleFor(act => act.EType)
+                .NotEmpty()
+                .NotNull();
+
+            RuleFor(act => act.Ehost)
+                .NotEmpty()
+                .NotNull();
+
+        }
+    }
+}
